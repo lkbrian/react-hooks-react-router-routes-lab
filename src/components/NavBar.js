9bebf95f0ab3a1e439b7395a820eb496337dcf3f
lkbrian <lkbrian742@gmail.com>
2024-02-06 22:08:37 +0300

    completed assignment

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,8 +1,19 @@
 import React from "react";
 import { NavLink } from "react-router-dom";
-
+ const links = {
+display:"flex",
+flexDirection: "row",
+gap:"1rem"
+ }
 function NavBar() {
-  return <div>{/*{code here}*/}</div>;
+  return (
+    <div className="navbar" style={links}>
+      <NavLink  to="/">Home</NavLink>
+      <NavLink to="/movies">Movies</NavLink>
+      <NavLink to="/directors">Directors</NavLink>
+      <NavLink to="/actors">Actors</NavLink>
+    </div>
+  );
 }
 
 export default NavBar;
